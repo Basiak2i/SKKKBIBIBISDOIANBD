9fdea091516926933368762951d67ddf39366b0b
Basiak2i <102364757+Basiak2i@users.noreply.github.com>
2023-12-04 10:27:15 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+const sigma = document.querySelector(".sigma");
+const coffie_size = document.querySelector("#coffie_size");
+
+sigma.addEventListener("click ", function () {
+  coffie_size.classList.toggle("hidden");
+});
